3e01f33195fa4f36a2883811212d861c24f947ee
wxleah <32212478+wxleah@users.noreply.github.com>
2024-03-21 16:01:17 +0800

    fix router base

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,3 @@
-import { useState, useEffect, useRef } from 'react';
-import MovieList from './components/MovieList';
-import MovieListHeading from './components/MovieListHeading';
-import SearchBox from './components/SearchBox';
-import AddFavorites from './components/AddFavorites';
-import RemoveFavorites from './components/RemoveFavorites';
-import MovieModal from './components/MovieModal';
-import * as api from './api/api'
-import SearchNote from './components/SearchNote';
-import ViewMore from './components/ViewMore';
-
 // styling
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -18,6 +7,7 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from './components/Layout';
 import routes from "./routes"
+import { defineConfig } from 'vite';
 
 
 function App() {
@@ -28,42 +18,12 @@ function App() {
       // child route components
       children: routes,
     },
-  ])
+  ], { basename: import.meta.env.DEV ? '/' : defineConfig.base })
 
 
   return (
     <>
       <RouterProvider router={router} />
-
-      {/* <div className='container-fluid movie-app'>
-
-          <><div className="row d-flex align-items-center mt-4 mb-4">
-            <MovieListHeading heading="Movies" />
-            <SearchBox
-              value={searchValue}
-              handleSubmit={handleSearchSubmit}
-              handleChange={handleSearchChange} />
-          </div><div className='row'>
-              <MovieList movies={movies}
-                handleFavoritesClick={addFavoriteMovie}
-                favoriteComponent={AddFavorites} />
-              {movies.length !== 0 ?
-                <ViewMore onClick={handleViewMoreClick} /> : <SearchNote />}
-            </div></>
-
-
-          <div className="row d-flex align-items-center mt-4 mb-4">
-            <MovieListHeading heading="Favorites" />
-            <div className='row'>
-
-              <MovieList
-                movies={favorites}
-                handleFavoritesClick={RemoveFavoriteMovie}
-                favoriteComponent={RemoveFavorites}
-              />
-            </div>
-          </div>
-      </div> */}
     </>
   )
 }
